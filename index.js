350c6d2c04d854a5e9f644863070c8c9c47f4224
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-27 02:34:11 +0530

    update the index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4000;
 
 app.get('/' , (req  ,res) => {
-    res.send('hey , How are yoy Guys')
+    res.send('kya haal h jee kese ho sb')
 })
 
 app.listen(port , ()=>console.log(`server is started at port ${port}`))
